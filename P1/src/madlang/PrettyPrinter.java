48e9bf70c3f08cc77ef9decf905bd9a354e174aa
ssankar25 <awesomeunni25@gmail.com>
2026-02-04 19:15:23 -0600

    Passing tests 2 and 3 after implementing them

--- P1/src/madlang/PrettyPrinter.java
+++ P1/src/madlang/PrettyPrinter.java
@@ -70,44 +70,111 @@ public final class PrettyPrinter implements Expr.Visitor<String>, Stmt.Visitor<S
         }
         // fn name(params): {
         out.append("): ").append(funDecl.returnType.toSource()).append(" ");
-        printBlock(funDecl.body);
+        printBlock(funDecl.body, false);
     }
 
     private void printIndent() {
         for (int i = 0; i < indent; i++) out.append("  ");
     }
 
-    // Without leadng indent
-    private void printBlock(Stmt.Block block) {
+    // LeadingIndent flag passed in to take care of in-line blocks vs. standalone blocks
+    private void printBlock(Stmt.Block b, boolean leadingIndent) {
+        if (leadingIndent) printIndent();
         out.append("{\n");
         indent++;
-        for (Stmt s : block.stmts) s.accept(this);
+        for (Stmt s : b.stmts) s.accept(this);
         indent--;
         printIndent();
         out.append("}\n");
     }
 
+    // Prints as a block if the statement is a block, otherwise it is a single statement
+    // printed on the next line
+    private void printBody(Stmt body) {
+        if (body instanceof Stmt.Block b) {
+            // In-line block statement
+            printBlock(b, false);
+        } else {
+            out.append("\n");
+            indent++;
+            body.accept(this);
+            indent--;
+        }
+    }
+
     @Override
     public String visitBlockStmt(Stmt.Block s) {
-        // For now, only needed if blocks appear inside statements (if/while)
-        printIndent();
-        printBlock(s);
+        printBlock(s, true);
         return null;
     }
 
     @Override
     public String visitReturnStmt(Stmt.Return s) {
         printIndent();
+        // return;\n
         out.append("return ").append(s.value.accept(this)).append(";\n");
         return null;
     }
 
-    @Override public String visitVarDefStmt(Stmt.VarDef s) { throw new UnsupportedOperationException(); }
-    @Override public String visitFunDefStmt(Stmt.FunDef s) { throw new UnsupportedOperationException(); }
-    @Override public String visitAssignStmt(Stmt.Assign s) { throw new UnsupportedOperationException(); }
-    @Override public String visitIfStmt(Stmt.If s)         { throw new UnsupportedOperationException(); }
-    @Override public String visitWhileStmt(Stmt.While s)   { throw new UnsupportedOperationException(); }
-    @Override public String visitExprStmt(Stmt.ExprStmt s) { throw new UnsupportedOperationException(); }
+    @Override 
+    public String visitVarDefStmt(Stmt.VarDef s) { 
+        printIndent();
+        out.append(s.name).append(": ").append(s.type.toSource());
+        if (s.initOrNull != null) {
+            out.append(" = ").append(s.initOrNull.accept(this));
+        }
+        out.append(";\n");
+        return null;
+    }
+
+    @Override 
+    public String visitFunDefStmt(Stmt.FunDef s) { 
+        // printIndent is in FunDecl (by convention, any printIndent is in the innermost object)
+        printFunDecl(s.fun);
+        return null;
+    }
+
+    @Override 
+    public String visitAssignStmt(Stmt.Assign s) { 
+        printIndent();
+        // LHS = RHS;\n
+        out.append(s.name)
+           .append(" = ")
+           .append(s.rhs.accept(this))
+           .append(";\n");
+        return null;
+    }
+
+    @Override
+    public String visitExprStmt(Stmt.ExprStmt s) {
+        printIndent();
+        out.append(s.expr.accept(this)).append(";\n");
+        return null;
+    }
+
+    @Override
+    public String visitIfStmt(Stmt.If s) { 
+        printIndent();
+        out.append("if (").append(s.cond.accept(this)).append(") ");
+        printBody(s.thenBranch);
+
+        if (s.elseBranchOrNull != null) {
+            printIndent();
+            out.append("else ");
+            printBody(s.elseBranchOrNull);
+        }
+        return null;
+    }
+    
+    @Override
+    public String visitWhileStmt(Stmt.While s)   { 
+        printIndent();
+        out.append("while (").append(s.cond.accept(this)).append(") ");
+        printBody(s.body);
+        return null;
+    }
+
+    // Expression visitors return strings so that they can be used in the Statement visitors
 
     @Override
     public String visitIntLitExpr(Expr.IntLit e) {
@@ -121,8 +188,9 @@ public final class PrettyPrinter implements Expr.Visitor<String>, Stmt.Visitor<S
 
     @Override
     public String visitUnaryExpr(Expr.Unary e) {
-        // naive: always parenthesize operand if it's not an atom
         String inner = e.expr.accept(this);
+
+        // If binary expression, then need to include the parentheses without precedence checking
         boolean needParens = e.expr instanceof Expr.Binary;
         return e.op.toSource() + (needParens ? "(" + inner + ")" : inner);
     }
@@ -135,9 +203,25 @@ public final class PrettyPrinter implements Expr.Visitor<String>, Stmt.Visitor<S
         return "(" + l + " " + e.op.toSource() + " " + r + ")";
     }
 
-    @Override public String visitBoolLitExpr(Expr.BoolLit e) {
+    @Override 
+    public String visitBoolLitExpr(Expr.BoolLit e) {
         return Boolean.toString(e.value);
     }
 
-    @Override public String visitCallExpr(Expr.Call e)       { throw new UnsupportedOperationException(); }
+    @Override
+    public String visitCallExpr(Expr.Call e) {
+        StringBuilder sb = new StringBuilder();
+
+        // Calling function name
+        sb.append(e.callee).append("(");
+        for (int i = 0; i < e.args.size(); i++) {
+            // Add on arguments if they are there
+            if (i > 0) sb.append(", ");
+            sb.append(e.args.get(i).accept(this));
+        }
+    
+        sb.append(")");
+
+        return sb.toString();
+    }
 }
--- P1/src/madlang/test/TestPrograms.java
+++ P1/src/madlang/test/TestPrograms.java
@@ -73,9 +73,94 @@ public final class TestPrograms {
    * TODO: hard-code the AST for the example in README.md.
    */
   public static Ast.Program test2() {
-    throw new UnsupportedOperationException(
-      "TODO: test2: hard-code the AST for the example in README.md"
-    );
+
+    /// INNER FUNCTION /// 
+
+    Ast.Param yParam = new Ast.Param("y", Ast.Type.INT);
+
+    // r = r + 10
+    Expr.Binary innerAssignRExpr = new Expr.Binary(new Expr.Var("r"), Expr.BinOp.ADD, new Expr.IntLit(10));
+    Stmt.Assign innerAssignR = new Stmt.Assign("r", innerAssignRExpr);
+
+    // return y * y;
+    Expr.Binary innerReturnExpr = new Expr.Binary(new Expr.Var("y"), Expr.BinOp.MUL, new Expr.Var("y"));
+    Stmt.Return innerReturn = new Stmt.Return(innerReturnExpr);
+
+    // { r = r + 10; return y * y; }
+    Stmt.Block innerBody = new Stmt.Block(Arrays.asList(innerAssignR, innerReturn));
+
+    // fn Inner(y: int): int { r = r + 10; return y * y; }
+    Ast.FunDecl innerDecl = new Ast.FunDecl("Inner", Arrays.asList(yParam), Ast.Type.INT, innerBody);
+
+    // Need function as statement so it can go inside the Outer function
+    Stmt.FunDef innerDef = new Stmt.FunDef(innerDecl);
+
+    /// WHILE LOOP /// 
+
+    // (i < x)
+    Expr.Binary whileCondExpr = new Expr.Binary(new Expr.Var("i"), Expr.BinOp.LT, new Expr.Var("x"));
+
+    // i % 2
+    Expr.Binary ifCondExprMod = new Expr.Binary(new Expr.Var("i"), Expr.BinOp.MOD, new Expr.IntLit(2));
+    // (i % 2 == 0)
+    Expr.Binary ifCondExpr = new Expr.Binary(ifCondExprMod, Expr.BinOp.EQ, new Expr.IntLit(0));
+
+    // Inner(i)
+    Expr.Call callInner = new Expr.Call("Inner", Arrays.asList(new Expr.Var("i")));
+    // r + Inner(i)
+    Expr.Binary rPlusInner = new Expr.Binary(new Expr.Var("r"), Expr.BinOp.ADD, callInner);
+    // r = r + Inner(i)
+    Stmt.Assign thenAssignR = new Stmt.Assign("r", rPlusInner);
+    Stmt.Block thenBlock = new Stmt.Block(Arrays.asList(thenAssignR));
+
+    // if (i % 2 == 0) { 
+    //     r = r + Inner(i); 
+    // } 
+    Stmt.If ifStmt = new Stmt.If(ifCondExpr, thenBlock, null);
+
+    // i = i + 1
+    Expr.Binary iPlusOne = new Expr.Binary(new Expr.Var("i"), Expr.BinOp.ADD, new Expr.IntLit(1));
+    Stmt.Assign incI = new Stmt.Assign("i", iPlusOne);
+
+    // while (i < x) { 
+    //     if (i % 2 == 0) { 
+    //         r = r + Inner(i); 
+    //     } 
+    //     i = i + 1; 
+    // }
+    Stmt.Block whileBody = new Stmt.Block(Arrays.asList(ifStmt, incI));
+    Stmt.While whileStmt = new Stmt.While(whileCondExpr, whileBody);
+
+    /// OUTER FUNCTION ///
+
+    Ast.Param xParam = new Ast.Param("x", Ast.Type.INT);
+
+    // Function-level variables and return statement
+    Stmt.VarDef outerDefR = new Stmt.VarDef("r", Ast.Type.INT, new Expr.IntLit(0));
+    Stmt.VarDef outerDefI = new Stmt.VarDef("i", Ast.Type.INT, new Expr.IntLit(0));
+    Stmt.Return outerReturn = new Stmt.Return(new Expr.Var("r"));
+
+    // Outer function declaration
+    Stmt.Block outerBody = new Stmt.Block(Arrays.asList(outerDefR, outerDefI, innerDef, whileStmt, outerReturn));
+    Ast.FunDecl outerDecl = new Ast.FunDecl("Outer", Arrays.asList(xParam), Ast.Type.INT, outerBody);
+
+
+    /// MAIN FUNCTION /// 
+
+    // r: int = Outer(5)
+    Expr.Call callOuter = new Expr.Call("Outer", Arrays.asList(new Expr.IntLit(5)));
+    Stmt.VarDef mainDefR = new Stmt.VarDef("r", Ast.Type.INT, callOuter);
+
+    // output(r)
+    Stmt.ExprStmt outputR = new Stmt.ExprStmt(new Expr.Call("output", Arrays.asList(new Expr.Var("r"))));
+
+    // return 0
+    Stmt.Return mainReturn = new Stmt.Return(new Expr.IntLit(0));
+    
+    Stmt.Block mainBody = new Stmt.Block(Arrays.asList(mainDefR, outputR, mainReturn));
+    Ast.FunDecl mainDecl = new Ast.FunDecl("main", Arrays.asList(), Ast.Type.INT, mainBody);
+    
+    return new Ast.Program(Arrays.<Ast.Decl>asList(outerDecl, mainDecl));
   }
 
 /**
@@ -87,11 +172,72 @@ public final class TestPrograms {
  *    (e.g., variable name, function name, or parameter name).
  * 3. Both the source program and its pretty-printed output should
  *    contain no more than 20 lines of code.
+ * 
+ * fn fact(x: int) int {
+ *   if (x < 0)
+ *     return -1;
+ *   if (x == 0) {
+ *     return 1;
+ *   }
+ *   else {
+ *     {
+ *       return x * fact(x - 1);
+ *     }
+ *   }
+ * }
+ * 
+ * fn main(): int {
+ *   sankar5: int = fact(123);
+ *   output(sankar5);
+ *   return 0;
+ * }
+ * 
+ * TODO: Need to test multiple parameters, uninitialized global variable declaration, standalone block, multiple params in Expr.Call (function call w multiple params)
  */
 public static Ast.Program test3() {
-  throw new UnsupportedOperationException(
-    "TODO: test3: hard-code an AST that includes your NetID as an identifier"
-  );
+
+  Ast.Param xParam = new Ast.Param("x", Ast.Type.INT);
+
+  // if (x < 0) return -1;
+  Stmt.Return negOneReturn = new Stmt.Return(new Expr.Unary(Expr.UnOp.NEG, new Expr.IntLit(1)));
+  Expr.Binary firstIfCond = new Expr.Binary(new Expr.Var("x"), Expr.BinOp.LT, new Expr.IntLit(0));
+  Stmt.If firstIf = new Stmt.If(firstIfCond, negOneReturn, null);
+
+  // return x * fact(x - 1); 
+  Expr.Binary xMinusOne = new Expr.Binary(new Expr.Var("x"), Expr.BinOp.SUB, new Expr.IntLit(1));
+  Expr.Call callFactRecur = new Expr.Call("fact", Arrays.asList(xMinusOne));
+  Expr.Binary recurCall = new Expr.Binary(new Expr.Var("x"), Expr.BinOp.MUL, callFactRecur);
+  Stmt.Return recurReturn = new Stmt.Return(recurCall);
+
+  // else {...}
+  Stmt.Block elseReturnBlock = new Stmt.Block(Arrays.asList(new Stmt.Block(Arrays.asList(recurReturn)))); // Multiple levels of blocks
+
+  // if (x == 0) { return 1 } else {{...}}
+  Expr.Binary secondIfCond = new Expr.Binary(new Expr.Var("x"), Expr.BinOp.EQ, new Expr.IntLit(0));
+  Stmt.Return oneReturn = new Stmt.Return(new Expr.IntLit(1));
+  Stmt.Block secondThenBlock = new Stmt.Block(Arrays.asList(oneReturn));
+  Stmt.If secondIf = new Stmt.If(secondIfCond, secondThenBlock, elseReturnBlock);
+
+  Stmt.Block factBody = new Stmt.Block(Arrays.asList(firstIf, secondIf));
+  Ast.FunDecl factDecl = new Ast.FunDecl("fact", Arrays.asList(xParam), Ast.Type.INT, factBody);
+
+  /// MAIN FUNCTION ///
+
+  // sankar5: int = fact(123);
+  Expr.Call callFactMain = new Expr.Call("fact", Arrays.asList(new Expr.IntLit(123)));
+  Stmt.VarDef mainDefNetID = new Stmt.VarDef("sankar5", Ast.Type.INT, callFactMain);
+
+  // output(sankar5)
+  Stmt.ExprStmt outputNetID = new Stmt.ExprStmt(new Expr.Call("output", Arrays.asList(new Expr.Var("sankar5"))));
+
+  // return 0
+  Stmt.Return mainReturn = new Stmt.Return(new Expr.IntLit(0));
+  
+  Stmt.Block mainBody = new Stmt.Block(Arrays.asList(mainDefNetID, outputNetID, mainReturn));
+  Ast.FunDecl mainDecl = new Ast.FunDecl("main", Arrays.asList(), Ast.Type.INT, mainBody);
+  
+  return new Ast.Program(Arrays.<Ast.Decl>asList(factDecl, mainDecl));
+
 }
 
 }
